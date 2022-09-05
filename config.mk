73fb8648e3153d3c8e98d00404eb4a5188ff565e
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2022-09-05 17:13:22 +0300

    Makefile: Unbreak the build against DPDK headers

--- config.mk
+++ config.mk
@@ -4,14 +4,15 @@
 SPDK_DIR ?= /root/gleb/SPDK/spdk
 
 SPDK_HEADER_DIR=$(SPDK_DIR)/include
+DPDK_HEADER_DIR=$(SPDK_DIR)/dpdk/build/include
 
 SPDK_LIB_DIR=$(SPDK_DIR)/build/lib
-# DPDK_LIB_DIR= ???
+# DPDK_LIB_DIR= $(SPDK_DIR)/dpdk/build/lib
 VFIO_LIB_DIR=$(SPDK_DIR)/build/libvfio-user/usr/local/lib
 
 PKG_CONFIG_PATH = $(SPDK_LIB_DIR)/pkgconfig
 
-SPDK_CFLAGS+=-I$(SPDK_HEADER_DIR)
+SPDK_CFLAGS+=-I$(SPDK_HEADER_DIR) -I$(DPDK_HEADER_DIR)
 SPDK_CFLAGS+=-L$(SPDK_LIB_DIR) -L$(DPDK_LIB_DIR) -L$(VFIO_LIB_DIR)
 
 DPDK_LIB := $(shell PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" pkg-config --libs spdk_env_dpdk)
