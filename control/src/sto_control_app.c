52e7c21afac848985854a431713d6a4e2f94e30f
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2022-09-08 15:15:48 +0300

    control: Make the control app dummy

--- control/src/sto_control_app.c
+++ control/src/sto_control_app.c
@@ -1,89 +1,30 @@
-/*   SPDX-License-Identifier: BSD-3-Clause
- *   Copyright (c) Intel Corporation.
- *   All rights reserved.
- */
 #include <spdk/event.h>
 #include <spdk/log.h>
-#include <spdk/string.h>
-#include <spdk/likely.h>
-
-#include "sto_subprocess.h"
-
-static char *proc_name = "ls";
-static bool capture_output;
-
-/*
- * We'll use this struct to gather housekeeping proc_context to pass between
- * our events and callbacks.
- */
-struct proc_context_t {
-	char *name;
-};
-
-struct sto_subprocess_ctx subp_ctx;
 
 /*
  * Usage function for printing parameters that are specific to this application
  */
 static void
-proc_usage(void)
+control_usage(void)
 {
-	printf(" -P <proc name>                 name of the proc to execute\n");
-	printf(" -O <capture output>            if set subprocess will also capture the output (default 0)\n");
 }
 
 /*
  * This function is called to parse the parameters that are specific to this application
  */
 static int
-proc_parse_arg(int ch, char *arg)
+control_parse_arg(int ch, char *arg)
 {
-	switch (ch) {
-	case 'P':
-		proc_name = arg;
-		break;
-	case 'O':
-		capture_output = spdk_strtol(arg, 10);
-		break;
-	default:
-		return -EINVAL;
-	}
 	return 0;
-}
-
-static void
-proc_finish(struct sto_subprocess_ctx *subp_ctx)
-{
-	SPDK_NOTICELOG("Exec finish: rc=%d output=%s\n",
-		       subp_ctx->returncode, subp_ctx->output);
 }
 
 /*
  * Our initial event that kicks off everything from main().
  */
 static void
-proc_start(void *arg1)
+control_started(void *arg1)
 {
-	struct proc_context_t *proc_context = arg1;
-	struct sto_subprocess *subp;
-	const char *const argv[] = {
-		proc_context->name,
-	};
-	int rc;
-
-	SPDK_NOTICELOG("Successfully started the SPDK application: proc %s\n",
-		       proc_context->name);
-
-	subp_ctx.subprocess_done = proc_finish;
-
-	subp = sto_subprocess_create(argv, SPDK_COUNTOF(argv), capture_output, 0);
-	if (spdk_unlikely(subp == NULL)) {
-		SPDK_ERRLOG("Failed to create subprocess\n");
-		spdk_app_stop(-1);
-		return;
-	}
-
-	rc = sto_subprocess_run(subp, &subp_ctx);
+	SPDK_NOTICELOG("Successfully started the STO Control SPDK application\n");
 }
 
 int
@@ -91,29 +32,27 @@ main(int argc, char **argv)
 {
 	struct spdk_app_opts opts = {};
 	int rc = 0;
-	struct proc_context_t proc_context = {};
 
 	/* Set default values in opts structure. */
 	spdk_app_opts_init(&opts, sizeof(opts));
-	opts.name = "proc";
+	opts.name = "control";
 
 	/*
 	 * Parse built-in SPDK command line parameters as well
 	 * as our custom one(s).
 	 */
-	if ((rc = spdk_app_parse_args(argc, argv, &opts, "P:O:", NULL, proc_parse_arg,
-				      proc_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
+	if ((rc = spdk_app_parse_args(argc, argv, &opts, "", NULL, control_parse_arg,
+				      control_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
 		exit(rc);
 	}
-	proc_context.name = proc_name;
 
 	/*
-	 * spdk_app_start() will initialize the SPDK framework, call proc_start(),
+	 * spdk_app_start() will initialize the SPDK framework, call control_started(),
 	 * and then block until spdk_app_stop() is called (or if an initialization
 	 * error occurs, spdk_app_start() will return with rc even without calling
-	 * proc_start().
+	 * control_started().
 	 */
-	rc = spdk_app_start(&opts, proc_start, &proc_context);
+	rc = spdk_app_start(&opts, control_started, NULL);
 	if (rc) {
 		SPDK_ERRLOG("ERROR starting application\n");
 	}
