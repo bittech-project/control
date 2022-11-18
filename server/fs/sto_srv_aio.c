8e467cf377c66ab9ef595696f4b38318e32fe588
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2022-11-18 17:30:25 +0300

    server: fs: Add readfile support

--- server/fs/sto_srv_aio.c
+++ server/fs/sto_srv_aio.c
@@ -135,3 +135,153 @@ free_req:
 
 	return rc;
 }
+
+struct sto_srv_readfile_params {
+	char *filepath;
+	uint32_t size;
+};
+
+static void
+sto_srv_readfile_params_free(struct sto_srv_readfile_params *params)
+{
+	free(params->filepath);
+}
+
+static const struct spdk_json_object_decoder sto_srv_readfile_decoders[] = {
+	{"filepath", offsetof(struct sto_srv_readfile_params, filepath), spdk_json_decode_string},
+	{"size", offsetof(struct sto_srv_readfile_params, size), spdk_json_decode_uint32},
+};
+
+struct sto_srv_readfile_req {
+	struct sto_exec_ctx exec_ctx;
+
+	struct sto_srv_readfile_params params;
+	char *buf;
+
+	void *priv;
+	sto_srv_readfile_done_t done;
+};
+
+static int sto_srv_readfile_exec(void *arg);
+static void sto_srv_readfile_exec_done(void *arg, int rc);
+
+static struct sto_exec_ops srv_readfile_ops = {
+	.name = "readfile",
+	.exec = sto_srv_readfile_exec,
+	.exec_done = sto_srv_readfile_exec_done,
+};
+
+struct sto_srv_readfile_req *
+sto_srv_readfile_req_alloc(const struct spdk_json_val *params)
+{
+	struct sto_srv_readfile_req *req;
+
+	req = calloc(1, sizeof(*req));
+	if (spdk_unlikely(!req)) {
+		printf("server: Cann't allocate memory for readfile req\n");
+		return NULL;
+	}
+
+	if (spdk_json_decode_object(params, sto_srv_readfile_decoders,
+				    SPDK_COUNTOF(sto_srv_readfile_decoders), &req->params)) {
+		printf("server: Cann't decode readfile req params\n");
+		goto free_req;
+	}
+
+	sto_exec_init_ctx(&req->exec_ctx, &srv_readfile_ops, req);
+
+	return req;
+
+free_req:
+	free(req);
+
+	return NULL;
+}
+
+static void
+sto_srv_readfile_req_init_cb(struct sto_srv_readfile_req *req,
+			     sto_srv_readfile_done_t done, void *priv)
+{
+	req->done = done;
+	req->priv = priv;
+}
+
+static void
+sto_srv_readfile_req_free(struct sto_srv_readfile_req *req)
+{
+	sto_srv_readfile_params_free(&req->params);
+	free(req->buf);
+	free(req);
+}
+
+static int
+sto_srv_readfile_req_submit(struct sto_srv_readfile_req *req)
+{
+	return sto_exec(&req->exec_ctx);
+}
+
+static int
+sto_srv_readfile_exec(void *arg)
+{
+	struct sto_srv_readfile_req *req = arg;
+	struct sto_srv_readfile_params *params = &req->params;
+
+	if (!params->size) {
+		struct stat sb;
+
+		if (lstat(params->filepath, &sb) == -1) {
+			printf("server: Failed to get stat for file %s: %s\n",
+			       params->filepath, strerror(errno));
+			return -errno;
+		}
+
+		params->size = sb.st_size;
+	}
+
+	req->buf = calloc(1, params->size);
+	if (spdk_unlikely(!req->buf)) {
+		printf("server: Failed to alloc buf to read: size=%u\n", params->size);
+		return -ENOMEM;
+	}
+
+	return sto_read_file(params->filepath, req->buf, params->size);
+}
+
+static void
+sto_srv_readfile_exec_done(void *arg, int rc)
+{
+	struct sto_srv_readfile_req *req = arg;
+	char *buf = !rc ? req->buf : "";
+
+	req->done(req->priv, buf, rc);
+	sto_srv_readfile_req_free(req);
+}
+
+int
+sto_srv_readfile(const struct spdk_json_val *params,
+		 struct sto_srv_readfile_args *args)
+{
+	struct sto_srv_readfile_req *req;
+	int rc;
+
+	req = sto_srv_readfile_req_alloc(params);
+	if (spdk_unlikely(!req)) {
+		printf("server: Failed to alloc memory for readfile req\n");
+		return -ENOMEM;
+	}
+
+	sto_srv_readfile_req_init_cb(req, args->done, args->priv);
+
+	rc = sto_srv_readfile_req_submit(req);
+	if (spdk_unlikely(rc)) {
+		printf("server: Failed to submit readfile req, rc=%d\n", rc);
+		goto free_req;
+	}
+
+	return 0;
+
+free_req:
+	sto_srv_readfile_req_free(req);
+
+	return rc;
+}
--- server/include/sto_srv_aio.h
+++ server/include/sto_srv_aio.h
@@ -12,4 +12,14 @@ struct sto_srv_writefile_args {
 int sto_srv_writefile(const struct spdk_json_val *params,
 		      struct sto_srv_writefile_args *args);
 
+typedef void (*sto_srv_readfile_done_t)(void *priv, char *buf, int rc);
+
+struct sto_srv_readfile_args {
+	void *priv;
+	sto_srv_readfile_done_t done;
+};
+
+int sto_srv_readfile(const struct spdk_json_val *params,
+		     struct sto_srv_readfile_args *args);
+
 #endif /* _STO_SRV_AIO_H_ */
