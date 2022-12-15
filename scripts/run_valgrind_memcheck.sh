17f8dcfe211ee48d4f6195b88d8936185c96b6b4
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2022-12-15 12:35:33 +0300

    scripts/run_valgrind_memcheck.sh: Make the --track-origins argument settable

--- scripts/run_valgrind_memcheck.sh
+++ scripts/run_valgrind_memcheck.sh
@@ -1,2 +1,9 @@
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt ./control/src/control &&
+#!/bin/bash
+
+TRACK_ORIGINS="${1:-yes}"
+
+printf "Settings:
+	\t--track-origins=%s\n" "$TRACK_ORIGINS"
+
+valgrind --leak-check=full --show-leak-kinds=all --track-origins="$TRACK_ORIGINS" --log-file=valgrind-out.txt ./control/src/control &&
 cat valgrind-out.txt
