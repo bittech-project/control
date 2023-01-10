1b75a8ed99b4c5217b518c657862db327a520d5a
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2023-01-10 22:15:23 +0300

    control: main.c: Add control framework initialization

--- control/src/main.c
+++ control/src/main.c
@@ -3,13 +3,15 @@
 #include <spdk/likely.h>
 
 #include "sto_version.h"
+#include "sto_client.h"
+#include "sto_core.h"
 #include <sto_server.h>
 
 /*
  * Usage function for printing parameters that are specific to this application
  */
 static void
-control_usage(void)
+sto_control_usage(void)
 {
 }
 
@@ -17,7 +19,7 @@ control_usage(void)
  * This function is called to parse the parameters that are specific to this application
  */
 static int
-control_parse_arg(int ch, char *arg)
+sto_control_parse_arg(int ch, char *arg)
 {
 	return 0;
 }
@@ -26,10 +28,41 @@ control_parse_arg(int ch, char *arg)
  * Our initial event that kicks off everything from main().
  */
 static void
-control_started(void *arg1)
+sto_control_started(void *arg1)
 {
+	int rc = 0;
+
+	rc = sto_client_connect(STO_LOCAL_SERVER_ADDR, AF_UNIX);
+	if (rc < 0) {
+		SPDK_ERRLOG("sto_client_connect() failed, rc=%d\n", rc);
+		goto out_err;
+	}
+
+	rc = sto_core_init();
+	if (rc < 0) {
+		SPDK_ERRLOG("sto_core_init() failed, rc=%d\n", rc);
+		goto client_close;
+	}
+
 	SPDK_NOTICELOG("Successfully started the %s SPDK application\n",
 		       STO_VERSION_STRING);
+
+	return;
+
+client_close:
+	sto_client_close();
+
+out_err:
+	spdk_app_stop(rc);
+}
+
+static void
+sto_control_shutdown(void)
+{
+	sto_core_fini();
+	sto_client_close();
+
+	spdk_app_stop(0);
 }
 
 int
@@ -52,18 +85,20 @@ main(int argc, char **argv)
 	 * Parse built-in SPDK command line parameters as well
 	 * as our custom one(s).
 	 */
-	if ((rc = spdk_app_parse_args(argc, argv, &opts, "", NULL, control_parse_arg,
-				      control_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
+	if ((rc = spdk_app_parse_args(argc, argv, &opts, "", NULL, sto_control_parse_arg,
+				      sto_control_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
 		exit(rc);
 	}
 
+	opts.shutdown_cb = sto_control_shutdown;
+
 	/*
-	 * spdk_app_start() will initialize the SPDK framework, call control_started(),
+	 * spdk_app_start() will initialize the SPDK framework, call sto_control_started(),
 	 * and then block until spdk_app_stop() is called (or if an initialization
 	 * error occurs, spdk_app_start() will return with rc even without calling
-	 * control_started().
+	 * sto_control_started().
 	 */
-	rc = spdk_app_start(&opts, control_started, NULL);
+	rc = spdk_app_start(&opts, sto_control_started, NULL);
 	if (rc) {
 		SPDK_ERRLOG("ERROR starting application\n");
 	}
