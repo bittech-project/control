63ccd2e44af0478c09fd7c3307a11383942f412c
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2023-01-10 22:31:00 +0300

    mk/spdk.mk: Use SPDK submodule dir as default

--- mk/spdk.mk
+++ mk/spdk.mk
@@ -1,19 +1,19 @@
 #
 # Basic configuration options
 #
-SPDK_DIR ?= /root/gleb/spdk
+SPDK_DIR := $(CONTROL_ROOT_DIR)/spdk
 
-SPDK_HEADER_DIR=$(SPDK_DIR)/include
-DPDK_HEADER_DIR=$(SPDK_DIR)/dpdk/build/include
-
-SPDK_LIB_DIR=$(SPDK_DIR)/build/lib
-# DPDK_LIB_DIR= $(SPDK_DIR)/dpdk/build/lib
-VFIO_LIB_DIR=$(SPDK_DIR)/build/libvfio-user/usr/local/lib
+SPDK_HEADER_DIR ?= $(SPDK_DIR)/include
+SPDK_LIB_DIR ?= $(SPDK_DIR)/build/lib
+DPDK_HEADER_DIR ?= $(SPDK_DIR)/dpdk/build/include
 
 PKG_CONFIG_PATH = $(SPDK_LIB_DIR)/pkgconfig
 
-SPDK_CFLAGS+=-I$(SPDK_HEADER_DIR) -I$(DPDK_HEADER_DIR)
-SPDK_LDFLAGS+=-L$(SPDK_LIB_DIR) -L$(DPDK_LIB_DIR) -L$(VFIO_LIB_DIR)
+# DPDK_LIB_DIR= $(SPDK_DIR)/dpdk/build/lib
+# VFIO_LIB_DIR = $(SPDK_DIR)/build/libvfio-user/usr/local/lib
+
+SPDK_CFLAGS := -I$(SPDK_HEADER_DIR) -I$(DPDK_HEADER_DIR)
+SPDK_LDFLAGS := -L$(SPDK_LIB_DIR)
 
 DPDK_LIB := $(shell PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" pkg-config --libs spdk_env_dpdk)
 SPDK_EVENT_LIB := $(shell PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" pkg-config --libs spdk_event spdk_event_bdev)
