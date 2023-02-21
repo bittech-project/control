2e51dccadd8bc6d1bd6bae93b01b54f6694e211c
Gleb Chesnokov <gleb.chesnokov@bitblaze.tech>
2023-02-21 15:18:56 +0300

    control: async: Rename sto_generic_cb_ctx struct into sto_generic_cpl

--- control/include/sto_async.h
+++ control/include/sto_async.h
@@ -1,34 +1,49 @@
 #ifndef _STO_ASYNC_H_
 #define _STO_ASYNC_H_
 
+#include <spdk/stdinc.h>
+
 typedef void (*sto_generic_cb)(void *cb_arg, int rc);
 
-struct sto_generic_cb_ctx {
-	void *cb_arg;
+struct sto_generic_cpl {
 	sto_generic_cb cb_fn;
+	void *cb_arg;
 };
 
-static inline struct sto_generic_cb_ctx *
-sto_generic_cb_ctx_alloc(void *cb_arg, sto_generic_cb cb_fn)
+static inline struct sto_generic_cpl *
+sto_generic_cpl_alloc(sto_generic_cb cb_fn, void *cb_arg)
 {
-	struct sto_generic_cb_ctx *ctx;
+	struct sto_generic_cpl *cpl;
 
-	ctx = calloc(1, sizeof(*ctx));
-	if (spdk_unlikely(!ctx)) {
+	cpl = calloc(1, sizeof(*cpl));
+	if (spdk_unlikely(!cpl)) {
 		return NULL;
 	}
 
-	ctx->cb_arg = cb_arg;
-	ctx->cb_fn = cb_fn;
+	cpl->cb_arg = cb_arg;
+	cpl->cb_fn = cb_fn;
 
-	return ctx;
+	return cpl;
 }
 
 static inline void
-sto_generic_cb_ctx_done(struct sto_generic_cb_ctx *ctx, int rc)
+sto_generic_cpl_free(struct sto_generic_cpl *cpl)
 {
-	ctx->cb_fn(ctx->cb_arg, rc);
-	free(ctx);
+	free(cpl);
+}
+
+static inline void
+sto_generic_call_cpl(struct sto_generic_cpl *cpl, int rc)
+{
+	cpl->cb_fn(cpl->cb_arg, rc);
+
+	sto_generic_cpl_free(cpl);
+}
+
+static inline void
+sto_generic_cpl_cb(void *priv, int rc)
+{
+	sto_generic_call_cpl(priv, rc);
 }
 
 #endif /* _STO_ASYNC_H_ */
